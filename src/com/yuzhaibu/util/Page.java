4ddc00afcd128baadde4fa584fd0a10d992a2d99
wangkwei <wangkewei01@gmail.com>
2016-03-25 21:44:41 +0800

    page

--- src/com/yuzhaibu/util/Page.java
+++ src/com/yuzhaibu/util/Page.java
@@ -1,5 +1,6 @@
 package com.yuzhaibu.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Page {
@@ -16,8 +17,69 @@ public class Page {
 		this.count = count;
 		this.pageSize = pageSize;
 		this.currentPage = currentPage;
+		
+		pageCount = calPageCount(count,pageSize);
+		pageList = calPageList(currentPage,pageCount);
+		prvPage = calPrePage(currentPage);
+		nextPage = calNtPage(currentPage, pageList);
 	}
 	
+	private Integer calPageCount(Integer c,Integer s){
+		Integer pagec=1;
+		if(c/s!=0){
+			if(c%s!=0){
+				pagec = c/s+1;
+			}else{
+				pagec = c/s;
+			}
+		}else{
+			pagec=1;
+		}
+		
+		return pagec;
+	}
+	
+	private List<Integer> calPageList(Integer cur,Integer pc){
+		List<Integer> li = new ArrayList<Integer>();
+		if(pc<=5){
+			for(int i=1;i<=pc;i++){
+				li.add(i);
+			}
+		}else{
+			if(cur<=pc-5){
+				int index = cur%5;
+				int cou = cur/5;
+				for(int i=5*cou+1;i<=5*cou+5;i++){
+					li.add(i);
+				}
+			}else{
+				int index = cur%5;
+				int cou = pc%5;
+				for(int i=pc-cou+1;i<=pc;i++){
+					li.add(i);
+				}
+			}
+			
+			
+		}
+		return li;
+	}
+	
+	private Integer calPrePage(Integer cur){
+		if(cur==1){
+			return 1;
+		}else{
+			return cur-1;
+		}
+	}
+	
+	private Integer calNtPage(Integer cur,List<Integer> pageList){
+		if(pageList.lastIndexOf(cur.intValue())!=pageList.size()-1){
+			return cur+1;
+		}else{
+			return cur;
+		}
+	}
 	
 	public Integer getCount() {
 		return count;
